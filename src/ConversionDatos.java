29130537e65681e4d7ed6740739e2506cab7c314
4527-Priscilla <priscilla.arayac@gmail.com>
2025-11-14 01:19:18 -0300

    Actualización código

--- src/ConversionDatos.java
+++ src/ConversionDatos.java
@@ -4,15 +4,15 @@
 
 public class ConversionDatos {
 
-    // 1. Variable Privada: El nombre 'conversion_rate' debe coincidir EXACTAMENTE 
+    // Variable Privada: El nombre 'conversion_rate' debe coincidir EXACTAMENTE
     //    con el campo en el JSON de la API para que Gson lo reconozca.
     private double conversion_rate;
 
-    // 2. Constructor vacío para la inicialización por Gson
+    // Constructor vacío para la inicialización por Gson
     public ConversionDatos() {
     }
 
-    // 3. Método Getter: Permite a la clase ConversorApp acceder al valor privado.
+    // Getter: Permite a la clase ConversorApp acceder al valor privado.
     public double getConversionRate() {
         return conversion_rate;
     }
